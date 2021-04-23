8b9e2614419d301a9659919a2aa2585bf1349e04
GentleSmile <478691929@qq.com>
2021-04-23 18:02:46 +0800

    support CI/CD

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # THU-MARS-Web
 
-## 使用方式
+Preview: http://101.6.96.177:9526/
+
+TODO:
+- [ ] add SSL certificate
+- [ ] show CI/CD on github
+
+## Usage
+
+将修改的内容push到主分支之后，网站会自动加载修改的内容，一分钟之后Preview会更新，github对服务器的访问超时或者服务器对github的访问超时都会导致Preview无法及时更新。
+
 
 ### 文字修改
 例如，当需要修改下图中的Multimedia的介绍时，在main.js中找到overview_of_research_directions→content，修改对应的值即可。
 
+> 如果内容中包含引号，需要在引号前添加转义字符 '\'
+
 ### 图片修改
 例如，当需要修改下图中的Multimedia的图片时，将想要的图片放在images目录下，然后在main.js中找到overview_of_research_directions→image，将对应的值修改为图片的文件名。
 
-![img.png](img.png)
+![img_1.png](img.png)
--- main.js
+++ main.js
@@ -1,67 +1,91 @@
-var main = {
-    about_our_lab: 'MARS Lab is an interdisciplinary AI lab working on a range of moonshot problems. Making impossible possible, our goal is the beyond the Mars.',
+const main = {
+  title: 'We’re building the World’s Most Experienced MARS Lab',
 
-    overview_of_research_directions: [
-        {
-            content: 'Multimedia',
-            image: 'multimedia.png'
-        },
-        {
-            content: 'Autonomous Vehicles',
-            image: 'autonomous_vehicles.png'
-        },
-        {
-            content: 'Robotics',
-            image: 'robotics.png'
-        },
-        {
-            content: 'Sensors',
-            image: 'sensors.png'
-        },
-    ],
+  about_our_lab: 'MARS Lab is an interdisciplinary AI lab working on a range of moonshot problems. Making impossible possible, our goal is the beyond the Mars.',
 
-    about_our_leader: [
-        'Dr. Hang Zhao is an Assistant Professor at IIIS, Tsinghua University. His research interests include multi-modal machine learning, computer vision and autonomous driving.',
-        'Dr. Zhao was a Research Scientist at Waymo (known as Google\'s self-driving project). Before that, he got his Ph.D. degree at MIT under Professor Antonio Torralba.'
-    ],
+  overview_of_research_directions: [
+    {
+      title: 'Multimedia',
+      content: 'Multimedia is',
+      image: 'multimedia.png'
+    },
+    {
+      title: 'Autonomous Vehicles',
+      content: 'Autonomous Vehicles is',
+      image: 'autonomous_vehicles.png'
+    },
+    {
+      title: 'Robotics',
+      content: 'Robotics is',
+      image: 'robotics.png'
+    },
+    {
+      title: 'Sensors',
+      content: 'Sensors is',
+      image: 'sensors.png'
+    },
+  ],
 
-    graduate_students: [
-        {
-            name: 'Chenzhuang Du',
-            website: 'https://github.com/Tsinghua-MARS-Lab/',
-        },
-        {
-            name: 'Junru Gu',
-            website: 'https://github.com/Tsinghua-MARS-Lab/',
-        },
-        {
-            name: 'Chenxu Hu',
-            website: 'https://github.com/Tsinghua-MARS-Lab/',
-        },
-        {
-            name: 'Wenxiao Wang',
-            website: 'https://github.com/Tsinghua-MARS-Lab/',
-        },
-        {
-            name: 'Yichen Liu',
-            website: 'https://github.com/Tsinghua-MARS-Lab/',
-        },
-    ],
-    
-    research_assistants: [
-        {
-            name: 'Zihui Xue',
-            website: 'https://github.com/Tsinghua-MARS-Lab/',
-        },
-        {
-            name: 'Tingle Li',
-            website: 'https://github.com/Tsinghua-MARS-Lab/',
-        },
-        {
-            name: 'Tianyu Hua',
-            website: 'https://github.com/Tsinghua-MARS-Lab/',
-        },
-    ],
-    
-    alumni: [],
+  about_our_leader: [
+    'Dr. Hang Zhao is an Assistant Professor at IIIS, Tsinghua University. His research interests include multi-modal machine learning, computer vision and autonomous driving.',
+    'Dr. Zhao was a Research Scientist at Waymo (known as Google\'s self-driving project). Before that, he got his Ph.D. degree at MIT under Professor Antonio Torralba.'
+  ],
+
+  graduate_students: [
+    {
+      name: 'Chenzhuang Du',
+      website: '',
+      content: 'Chenzhuang DuChenzhuang DuChenzhuang DuChenzhuang DuChenzhuang DuChenzhuang DuvChenzhuang Du',
+      image: '',
+    },
+    {
+      name: 'Junru Gu',
+      website: 'https://github.com/GentleSmile',
+      content: 'Junru Gu is a fourth year undergraduate student at Tsinghua University. He will be a graduate student in the fall of 2021.',
+      image: 'gujunru.png',
+    },
+    {
+      name: 'Chenxu Hu',
+      website: '',
+      content: 'Dr. Hang Zhao is an Assistant Professor at IIIS, Tsinghua University. His research interests include multi-modal machine learning, computer vision and autonomous driving.',
+      image: '',
+    },
+    {
+      name: 'Wenxiao Wang',
+      website: '',
+      content: '',
+      image: '',
+    },
+    {
+      name: 'Yichen Liu',
+      website: '',
+      content: '',
+      image: '',
+    },
+  ],
+
+  research_assistants: [
+    {
+      name: 'Zihui Xue',
+      website: 'https://github.com/Tsinghua-MARS-Lab/',
+      content: '',
+      image: '',
+    },
+    {
+      name: 'Tingle Li',
+      website: 'https://github.com/Tsinghua-MARS-Lab/',
+      content: '',
+      image: '',
+    },
+    {
+      name: 'Tianyu Hua',
+      website: 'https://github.com/Tsinghua-MARS-Lab/',
+      content: '',
+      image: '',
+    },
+  ],
+
+  alumni: [],
 }
+
+export default main
