2b4682d2377289aa6a53c82a00b61ba13c162bd4
Hang Zhao <zhaohang0124@gmail.com>
2021-04-29 12:48:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const main = {
   title: 'Our Missions are Beyond Mars',
 
   about_our_lab: [
-    'MARS Lab is an interdisciplinary AI lab at IIIS, Tsinghua University, led by Professor Hang Zhao. We are working on a range of moonshot problems, our current research focus on the fundamentals and applications of Multi-modal Learning: (1) Multimedia, (2) Autonomous Driving, (3) Robotics, (4) Sensors.',
+    'MARS Lab is an interdisciplinary AI lab at IIIS, Tsinghua University, led by <a href="http://people.csail.mit.edu/hangzhao/">Professor Hang Zhao</a>. We are working on a range of moonshot problems, our current research focus on the fundamentals and applications of Multi-modal Learning: (1) Multimedia, (2) Autonomous Driving, (3) Robotics, (4) Sensors.',
     'MARS Lab多模态学习实验室，是清华大学交叉信息院下的交叉学科人工智能实验室，由赵行教授领导。我们尝试解决一系列探索性的AI问题，当前我们主要研究多模态学习的基础到应用：(1)多媒体, (2)自动驾驶, (3)机器人, (4)多传感器。',
   ],
 
