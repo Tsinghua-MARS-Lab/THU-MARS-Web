f58b9729d7b65b5017bff12a3f7226eef9a81756
GentleSmile <478691929@qq.com>
2021-04-30 09:52:01 +0800

    Add files via upload

---
+++ about_our_lab.vue
@@ -0,0 +1,22 @@
+<template>
+  <div style="line-height: 23px; font-size: 100%">
+    <div style="margin-bottom: 10px">
+      MARS Lab is an interdisciplinary AI lab at IIIS, Tsinghua University, led by
+      <el-link type="primary" href="http://people.csail.mit.edu/hangzhao/" target="_blank">Professor Hang Zhao</el-link>
+      . We are working on a range of moonshot problems, our current research focus on the fundamentals and applications of Multi-modal Learning: (1) Multimedia, (2) Autonomous Driving, (3) Robotics, (4) Sensors.
+    </div>
+    <div style="margin-bottom: 10px">
+      MARS Lab多模态学习实验室，是清华大学交叉信息院下的交叉学科人工智能实验室，由赵行教授领导。我们尝试解决一系列探索性的AI问题，当前我们主要研究多模态学习的基础到应用：(1)多媒体, (2)自动驾驶, (3)机器人, (4)多传感器。
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about_our_lab.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
