57b552bc9f2b89e09ff752814a3fc2f6bcf71a8c
Hang Zhao <zhaohang0124@gmail.com>
2021-07-14 00:29:01 +0800

    Update about_our_lab.vue

--- about_our_lab.vue
+++ about_our_lab.vue
@@ -3,10 +3,13 @@
     <div style="margin-bottom: 10px">
       MARS Lab is an interdisciplinary AI lab at IIIS, Tsinghua University, led by
       <el-link type="primary" href="http://people.csail.mit.edu/hangzhao/" target="_blank">Professor Hang Zhao</el-link>
-      . We are working on a range of moonshot problems, and are always looking for new challenges. Our current research focus on the fundamentals and applications of Multi-modal Learning: (1) Multimedia Computing, (2) Autonomous Driving, (3) Robotics, (4) Sensors.
+      . We are working on a range of moonshot problems, and are always looking for new challenges.
+      Currently, we are especially interested in how to enable machines to learn, reason and interact from multi-modal inputs, just like human beings.
+      Our research spans the fundamentals and applications of Multi-modal Learning: (1) Multimedia Computing, (2) Autonomous Driving, (3) Robotics, (4) Sensors.
     </div>
     <div style="margin-bottom: 10px">
-      MARS Lab多模态学习实验室，是清华大学交叉信息院下的交叉学科人工智能实验室，由赵行教授组建和指导。我们尝试解决一系列探索性的AI问题，并且一直在寻找新的挑战。当前我们主要关注多模态学习的基础问题及其应用：(1)多媒体计算, (2)自动驾驶, (3)机器人, (4)多传感器。
+      MARS Lab多模态学习实验室，是清华大学交叉信息院下的交叉学科人工智能实验室，由赵行教授组建和指导。我们尝试解决一系列探索性的AI问题，并且一直在寻找新的挑战。
+      当前我们特别感兴趣如何让机器像人一样的能够通过多种感知输入进行学习、推理和交互。我们的研究涵盖了多模态学习的基础问题及其应用：(1)多媒体计算, (2)自动驾驶, (3)机器人, (4)多传感器。
     </div>
   </div>
 </template>
