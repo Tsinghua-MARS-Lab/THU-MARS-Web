774e7399afe2eaa0b7d409ccc723a09772a0c1d6
GentleSmile <478691929@qq.com>
2021-07-30 21:11:19 +0800

    Update publications.vue

--- publications.vue
+++ publications.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
     <publication paper_link="https://arxiv.org/abs/2008.08294">
-      <div class="title">TNT: Target-driveN Trajectory Prediction</div>
+      <b>TNT: Target-driveN Trajectory Prediction</b>
       Hang Zhao*, Jiyang Gao*, Tian Lan, Chen Sun, Benjamin Sapp, Balakrishnan Varadarajan, Yue Shen, Yi Shen, Yuning
-      Chai, Cordelia Schmid, Congcong Li, Dragomir Anguelov<br/>
-      Conference on Robot Learning (<b>CoRL</b>)<br/>
-      Boston (virtual), Nov. 2020<br/>
-      "A new SOTA multi-trajectory prediction model."<br/>
+      Chai, Cordelia Schmid, Congcong Li, Dragomir Anguelov
+      Conference on Robot Learning (<b>CoRL</b>)
     </publication>
 
     <publication paper_link="https://arxiv.org/abs/2008.08294">
-      <div class="title">VectorNet: Encoding HD Maps and Agent Dynamics from Vectorized Representation</div>
-      Jiyang Gao*, Chen Sun*, Hang Zhao, Yi Shen, Dragomir Anguelov, Congcong Li, Cordelia Schmid<br/>
-      In Proc. Computer Vision and Pattern Recognition (<b>CVPR</b>)<br/>
-      Seattle (virtual), June. 2020<br/>
-      "A graphical representation of HD maps."<br/>
+      <b>VectorNet: Encoding HD Maps and Agent Dynamics from Vectorized Representation</b>
+      Jiyang Gao*, Chen Sun*, Hang Zhao, Yi Shen, Dragomir Anguelov, Congcong Li, Cordelia Schmid
+      In Proc. Computer Vision and Pattern Recognition (<b>CVPR</b>)
     </publication>
   </div>
 </template>
